b6881cbf244fce6c9db4afb543758027352fc6a8
ginniehench_dsk_tp <ginniehench@gmail.com>
2017-12-18 22:57:04 -0500

    User index page now displays
    Also, add 'get 'profile' for users#show' as an alternate to users/:id in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # get 'home/index'
 
   devise_for :users
-  get 'users/:id' => 'users#show', as: :user
+  get 'users/' => 'users#index'
+  get 'users/:id' => 'users#show'
+  get 'profile' => 'users#show'
   root to: "home#index"
 end
